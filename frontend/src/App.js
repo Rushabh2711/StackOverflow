02a5726f282f6341253107f34e519dfcc5bb2178
Rushabh2711 <shetarushabh@gmail.com>
2022-04-27 11:39:05 -0700

    addtag and home page added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,8 @@ import SignUp from "./pages/SignUp";
 import Login from "./pages/Login";
 import UserProfile from "./pages/UserProfile";
 import UserActivity from "./pages/UserActivity";
+import AdminHome from "./pages/admin/AdminHome";
+import AdminDashboard from "./pages/admin/AdminDashboard";
 
 function App() {
   return (
@@ -14,6 +16,8 @@ function App() {
           <Route path="/signup" element={<SignUp />} />
           <Route path="/users/profile/:id" element={<UserProfile />} />
           <Route path="/users/activity/:id" element={<UserActivity />} />
+          <Route path="/adminHome" element={<AdminHome />} />
+          <Route path="/adminDashboard" element={<AdminDashboard />} />
         </Routes>
       </BrowserRouter>
     </div>
