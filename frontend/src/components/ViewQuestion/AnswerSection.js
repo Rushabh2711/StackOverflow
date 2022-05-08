c0cdef784a179a7428e17e5f424d61829a193da5
vdbhanderi <virag.bhanderi@sjsu.edu>
2022-05-08 12:43:48 -0700

    Update answer section

--- frontend/src/components/ViewQuestion/AnswerSection.js
+++ frontend/src/components/ViewQuestion/AnswerSection.js
@@ -50,7 +50,7 @@ export default function Answer(props) {
             <p className="arrow" style={{ "fontSize": "1.3rem" }}>{parseInt(answer?.upvotes) - parseInt(answer?.downvotes)}</p>
 
             <p className="arrow votes" name="Downvote" onClick={votePost}>▼</p>
-
+            <svg aria-hidden="true" class="svg-icon iconCheckmarkLg" width="36" height="36" viewBox="0 0 36 36"><path d="m6 14 8 8L30 6v8L14 30l-8-8v-8Z"></path></svg>
             {/* <svg aria-hidden="true"  class="svg-icon iconBookmark" width="18" height="18" viewBox="0 0 18 18"><path d="M6 1a2 2 0 0 0-2 2v14l5-4 5 4V3a2 2 0 0 0-2-2H6Zm3.9 3.83h2.9l-2.35 1.7.9 2.77L9 7.59l-2.35 1.7.9-2.76-2.35-1.7h2.9L9 2.06l.9 2.77Z"></path></svg> */}
           </div>
         </div>
