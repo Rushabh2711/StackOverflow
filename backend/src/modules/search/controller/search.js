553288d89c21f89dd24b25ba78785c9edd77b050
Rushabh2711 <shetarushabh@gmail.com>
2022-05-10 10:07:15 -0700

    search api logic added

--- backend/src/modules/search/controller/search.js
+++ backend/src/modules/search/controller/search.js
@@ -1,108 +1,109 @@
 import { make_request } from "../../../../kafka/client.js";
-import Questions from "../../../db/models/mongo/question.js";
+import Posts from "../../../db/models/mongo/posts.js";
 import QuestionViews from "../../../db/models/mongo/questionViews.js";
 
 class SearchController {
 
     fetchSearchResult = async (req, res) => {
-        const text = "[java] user:12345 \"tex\" is:question isaccepted:yes";
+        const text = "Signed";
         // req.params.searchText;
         // "[java] user:12345 \"tex\" is:question isaccepted:yes";
         var results = [];
         const mySearchArray = text.split(" ");
         try {
-            let questions = await Questions.find({});
+            let posts = await Posts.find({});
+            // console.log(posts);
             mySearchArray.forEach(s => {
+
+                // search by tag
                 if(s.includes("[")) {
 
                     var tag = s.substring(1,s.length - 1);
                     console.log(tag);
-                    questions.forEach((q) => {
-                        if(q.tags.includes(tag)) {
-                            results.push(q);
+                    posts.forEach((q) => {
+                        if(q.postType === "question") {
+                            q.questionTags.forEach((t) => {
+                                if(t.name === tag) {
+                                    results.push(q);
+                                }
+                            })
                         }
                     })
 
                 }
+
+                // search by user
                 else if(s.includes("user:")) {
 
                     var id = s.substring(5, s.length);
                     console.log(id);
-                    questions.forEach((q) => {
+                    posts.forEach((q) => {
                         if(q.userId === id) {
-                            results.push({...q});
-                        }
-                        else{
-                            q.answers.forEach((a) => {
-                                if(a.userId === id) {
-                                    results.push({...q});
-                                }
-                            })
+                            results.push(q);
                         }
                     });
 
                 }
+
+                // is: type
                 else if(s.includes("is:question")) {
 
-                    results.push({...questions});
+                    posts.forEach((q) => {
+                        if(q.postType === "question") {
+                            results.push(q);
+                        }
+                    });
 
                 }
                 else if(s.includes("is:answer")) {
                     
-                    questions.forEach((q) => {
-                        if(q.answers.length > 0) {
-                            results.push({...q});
+                    posts.forEach((q) => {
+                        if(q.postType === "answer") {
+                            results.push(q);
                         }
-                    })
+                    });
 
                 }
+
+                // isaccepted type
                 else if(s.includes("isaccepted:yes")) {
 
-                    questions.forEach((q) => {
-                        q.answers.forEach((a) => {
-                            if(a.isBest) {
-                                results.push({...q});
-                                return;
-                            }
-                        })
-                    })
+                    posts.forEach((q) => {
+                        if(q.isAccepted) {
+                            results.push(q);
+                        }
+                    });
                     
                 }
                 else if(s.includes("isaccepted:no")) {
                     
-                    questions.forEach((q) => {
-                        q.answers.forEach((a) => {
-                            if(a.isBest) {
-                                results.push({...q});
-                            }
-                        })
-                    })
+                    posts.forEach((q) => {
+                        if(!q.isAcceptedAnswerId) {
+                            results.push(q);
+                        }
+                    });
 
                 }
+
+                // search by string but with out space
+                // "java"  -> accepted
+                // "hello word" -> not accepted
                 else{
                     console.log(s);
-                    questions.forEach((q) => {
-                        console.log(q.description.includes(s));
-                        if(q.title.includes(s) || q.description.includes(s)) {
-                            results.push({...q});
-                        }
-                        else {
-                            q.answers.forEach((a) => {
-                                if(a.description.includes(s)) {
-                                    results.push({...q});
-                                }
-                            })
+                    posts.forEach((q) => {
+                        // console.log(q.description.includes(s));
+                        if(q.questionTitle.includes(s) || q.description.includes(s)) {
+                            results.push(q);
                         }
                     })
                 }
-                console.log("------change-------------");
                 console.log(results);
 
-                questions = [...results];
+                posts = [...results];
                 results = [];
             });
-            console.log("after filter");
-            res.status(200).send(questions);
+            console.log(posts.length);
+            res.status(200).send(posts);
   
         } catch (err) {
             console.error(err);
